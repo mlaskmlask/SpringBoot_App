e9b76b506e57038082f49f34b99187de72109f78
mlaskmlask <laskowska.kadry@gmail.com>
2021-11-23 22:51:26 +0100

    JDBC connection pt 2

--- src/main/java/com/it/database/impl/JDBCUserRepositoryImpl.java
+++ src/main/java/com/it/database/impl/JDBCUserRepositoryImpl.java
@@ -19,24 +19,25 @@ public class JDBCUserRepositoryImpl implements IUserRepository {
 
     @Override
     public User authenticate(User user) {
-        try {
-            String SQL = "SELECT * FROM tuser WHERE login=?";
-            PreparedStatement preparedStatement = this.connection.prepareStatement(SQL);
-            preparedStatement.setString(1, user.getLogin());
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (!resultSet.next()) {
-                return null;
-            }
-            User userFromDB = new User();
-            userFromDB.setId(resultSet.getInt("id"));
-            userFromDB.setName(resultSet.getString("name"));
-            userFromDB.setSurname(resultSet.getString("surname"));
-            userFromDB.setLogin(resultSet.getString("login"));
-            userFromDB.setPass(resultSet.getString("pass"));
 
-            if (userFromDB.getPass().equals(DigestUtils.md5Hex(user.getPass()))) {
-                return userFromDB;
-            }
+        User userFromDB = this.getUser(user.getLogin());
+        if (userFromDB != null && userFromDB.getPass().equals(DigestUtils.md5Hex(user.getPass()))) {
+            return userFromDB;
+        }
+        return null;
+    }
+
+    @Override
+    public User updateUserData(User user) {
+        try {
+            String SQL = "UPDATE tuser SET name=?, surname=? WHERE login=?";
+            PreparedStatement preparedStatement = this.connection.prepareStatement(SQL);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getSurname());
+            preparedStatement.setString(3, user.getLogin());
+            preparedStatement.executeUpdate();
+            return this.getUser(user.getLogin());
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -44,22 +45,17 @@ public class JDBCUserRepositoryImpl implements IUserRepository {
     }
 
     @Override
-    public User updateUserData(User user) {
-        try{
-            String SQL = "UPDATE tuser SET name=?, surname=? WHERE login=?";
+    public User updateUserPass(User user) {
+        try {
+            String SQL = "UPDATE tuser SET pass=? WHERE login=?";
             PreparedStatement preparedStatement = this.connection.prepareStatement(SQL);
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getSurname());
-            preparedStatement.setString(3, user.getLogin());
+            preparedStatement.setString(1, user.getPass());
+            preparedStatement.setString(2, user.getLogin());
             preparedStatement.executeUpdate();
-
+            return this.getUser(user.getLogin());
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-    }
-
-    @Override
-    public User updateUserPass(User user) {
         return null;
     }
 
@@ -95,5 +91,27 @@ public class JDBCUserRepositoryImpl implements IUserRepository {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
+
+    private User getUser(String login) {
+        try {
+            String SQL = "SELECT * FROM tuser WHERE login=?";
+            PreparedStatement preparedStatement = this.connection.prepareStatement(SQL);
+            preparedStatement.setString(1, login);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                return null;
+            }
+            User userFromDB = new User();
+            userFromDB.setId(resultSet.getInt("id"));
+            userFromDB.setName(resultSet.getString("name"));
+            userFromDB.setSurname(resultSet.getString("surname"));
+            userFromDB.setLogin(resultSet.getString("login"));
+            userFromDB.setPass(resultSet.getString("pass"));
+            return userFromDB;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 }
